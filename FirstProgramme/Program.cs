b446f818f143695924496ae3c3793528dc3d5b9c
samer <samer@SAMER-PC>
2020-01-17 22:58:10 +0200

    Comment to last edit

--- FirstProgramme/Program.cs
+++ FirstProgramme/Program.cs
@@ -5,7 +5,12 @@ namespace FirstProgramme
     class Program
     {
         static void Main(string[] args)
-        {
+        {   //This is my first programme
+            
+            //Today my first programme
+            
+            
+            
             Console.WriteLine("Hi World!");
             Console.ReadKey();
 
